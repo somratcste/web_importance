00a4346108143ceefdba1ea7ab80944dc36d69ac
unknown <somratcste@SOMRAT.(none)>
2015-09-18 11:10:06 +0600

    share facebook,twiiter and all others

---
+++ addshare.php
@@ -0,0 +1,10 @@
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4df14df26c87e5bd"></script>
+<script type="text/javascript">
+  addthis.layers({
+    'theme' : 'light',
+    'share' : {
+      'position' : 'left',
+      'numPreferredServices' : 
+    }   
+  });
+</script>
