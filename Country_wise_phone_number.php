774a8c0301b2c4884052a1d350672de00fd41103
unknown <somratcste@SOMRAT.(none)>
2015-09-18 11:19:19 +0600

    Country Wise with phone number

---
+++ Country_wise_phone_number.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/bootstrap-formhelpers.css" rel="stylesheet">
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            
+            // BP
+            ga('create', 'UA-45105464-1', 'bootstrapformhelpers.com');
+            // WLTD
+            ga('create', 'UA-47807801-1', 'bootstrapformhelpers.com', {'name': 'winMarkLTD'}); 
+            ga('send', 'pageview');
+            ga('winMarkLTD.send', 'pageview');
+
+</script>
+</head>
+<body>
+<div class="form-group">
+                    <label for="exampleInputPhone"> Country </br></br>
+                        <div class= id="example4">
+                          <form class="example form-inline">
+                          <select id="countries_phone1" class="form-control bfh-countries" data-country="US"></select>
+                          <br><br>
+                          <input type="text" class="form-control bfh-phone" data-country="countries_phone1">
+                          </form>  
+                        </div>
+                    </label>
+                  </div>
+
+<script src="js/jquery-1.10.2.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap-formhelpers.min.js"></script>
+
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4df14df26c87e5bd"></script>
+<script type="text/javascript">
+  addthis.layers({
+    'theme' : 'light',
+    'share' : {
+      'position' : 'left',
+      'numPreferredServices' : 
+    }   
+  });
+</script>
+	
+</body>
+</html>
